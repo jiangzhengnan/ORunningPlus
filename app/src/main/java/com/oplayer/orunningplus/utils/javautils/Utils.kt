ba248f2c77089050d0fdb21a73c1f2b81afb83d9
zh-rCN-BenNiu <tunnello@163.com>
2019-12-31 18:03:16 +0800

    补充上传

--- app/src/main/java/com/oplayer/orunningplus/utils/javautils/Utils.kt
+++ app/src/main/java/com/oplayer/orunningplus/utils/javautils/Utils.kt
@@ -10,14 +10,32 @@ class Utils {
          * @param key
          * @return
          */
-        open fun getRealmKey(key: String): ByteArray? {
-            var newKey = ""
-            for (i in 0..3) {
-                newKey = newKey + key
-            }
-            return newKey.toByteArray()
-        }
+//         fun getRealmKey(key: String): ByteArray? {
+//            var newKey = ""
+//            for (i in 0..3) {
+//                newKey = newKey + key
+//            }
+//            return newKey.toByteArray()
+//        }
 
+        /**
+         * 根据密钥长度补足密钥
+         *
+         * @return
+         */
+        open fun toMakekey(str: String, strLength: Int, `val`: String): String? {
+            var str = str
+            var strLen = str.length
+            if (strLen < strLength) {
+                while (strLen < strLength) {
+                    val buffer = StringBuffer()
+                    buffer.append(str).append(`val`)
+                    str = buffer.toString()
+                    strLen = str.length
+                }
+            }
+            return str
+        }
 
     }
 
